f8b91689f12302f74c1bfb8e5d0a2c12ad110a64
giangnn-bkace <giangnn.bkace@gmail.com>
2018-10-06 19:58:34 +0900

    momentum

--- finetune_momentum.py
+++ finetune_momentum.py
@@ -194,8 +194,8 @@ def main(dataset='clipped_data', mode='rgb', split=1, investigate=0):
     global_index = tf.Variable(0, trainable=False)
 
     # Set learning rate schedule by hand, also you can use an auto way
-    boundaries = [per_epoch_step*5, per_epoch_step*10, per_epoch_step*15, per_epoch_step*20, per_epoch_step*25]
-    values = [_LEARNING_RATE, _LEARNING_RATE/2, _LEARNING_RATE/4, _LEARNING_RATE/8, _LEARNING_RATE/16, _LEARNING_RATE/32]
+    boundaries = [per_epoch_step*10, per_epoch_step*20]
+    values = [_LEARNING_RATE, _LEARNING_RATE/2, _LEARNING_RATE/4]
     learning_rate = tf.train.piecewise_constant(
         global_index, boundaries, values)
     
